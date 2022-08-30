6777ed575c04a29c8057760746397fe5ba748c7d
Rizki Rian Anandita <65359346+rizriana@users.noreply.github.com>
2022-08-30 13:08:27 +0700

    Create search heroes endpoint

--- src/main/kotlin/com/secondlab/plugins/Routing.kt
+++ src/main/kotlin/com/secondlab/plugins/Routing.kt
@@ -2,6 +2,7 @@ package com.secondlab.plugins
 
 import com.secondlab.routes.getAllHeroes
 import com.secondlab.routes.root
+import com.secondlab.routes.searchHeroes
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
 import io.ktor.server.routing.*
@@ -11,6 +12,7 @@ fun Application.configureRouting() {
     routing {
         root()
         getAllHeroes()
+        searchHeroes()
 
         static("/images") {
             resources("images")
---
+++ src/main/kotlin/com/secondlab/routes/SearchHeroes.kt
@@ -0,0 +1,22 @@
+package com.secondlab.routes
+
+import com.secondlab.repository.HeroRepository
+import com.secondlab.repository.HeroRepositoryImpl
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import org.koin.ktor.ext.inject
+
+fun Route.searchHeroes() {
+    val heroRepository: HeroRepository by inject()
+
+    get("/boruto/heroes/search") {
+        val name = call.request.queryParameters["name"]
+        val apiResponse = heroRepository.searchHeroes(name = name)
+        call.respond(
+            message = apiResponse,
+            status = HttpStatusCode.OK
+        )
+    }
+}
